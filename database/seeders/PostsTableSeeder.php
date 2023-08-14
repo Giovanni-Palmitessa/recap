263245677abdbef11d503aea3b81492188407639
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-08-14 16:32:29 +0200

    make migration and seeder

--- database/seeders/PostsTableSeeder.php
+++ database/seeders/PostsTableSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Tag;
 use App\Models\Post;
 use Faker\Generator as Faker;
 use Illuminate\Database\Seeder;
@@ -17,7 +18,9 @@ class PostsTableSeeder extends Seeder
     public function run(Faker $faker)
     {
         for ($i = 0; $i < 10; $i++) {
+            $tags = Tag::all();
             Post::create([
+                'tag_id' => $faker->randomElement($tags)->id,
                 'titolo' => $faker->words(rand(1, 4), true),
                 'descrizione' => $faker->paragraph(rand(2, 10), true),
             ]);
