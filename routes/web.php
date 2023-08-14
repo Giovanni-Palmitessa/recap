931c6494b86e01cfa93a9923a814c3091c8cac5e
alessandropecchini99 <pecchini99@gmail.com>
2023-08-14 16:40:09 +0200

    add tags routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Admin\TagController;
 use App\Http\Controllers\Admin\PostController;
 use App\Http\Controllers\Admin\PageController as AdminPageController;
 use App\Http\Controllers\Guest\PageController as guestPageController;
@@ -27,6 +28,7 @@ Route::middleware(['auth', 'verified'])
     ->group(function () {
         Route::get('/', [AdminPageController::class, 'dashboard'])->name('dashboard');
         Route::resource('posts', PostController::class);
+        Route::resource('tags', TagController::class);
     });
 
 Route::middleware('auth')
