5ea16abba90fd9cc00ded2737710371f46f0809f
alessandropecchini99 <pecchini99@gmail.com>
2023-08-16 11:32:54 +0200

    push

--- database/seeders/PostsTableSeeder.php
+++ database/seeders/PostsTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Tag;
 use App\Models\Post;
+use App\Models\Technology;
 use Faker\Generator as Faker;
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -17,13 +18,17 @@ class PostsTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
+        $tags = Tag::all();
+        $technologies = Technology::all()->pluck('id');
+
         for ($i = 0; $i < 10; $i++) {
-            $tags = Tag::all();
-            Post::create([
+            $post = Post::create([
                 'tag_id' => $faker->randomElement($tags)->id,
                 'titolo' => $faker->words(rand(1, 4), true),
                 'descrizione' => $faker->paragraph(rand(2, 10), true),
             ]);
+            // associare il post ad un certo numero di technologies
+            $post->technologies()->sync($faker->randomElements($technologies, rand(1, 3)));
         };
     }
 }
