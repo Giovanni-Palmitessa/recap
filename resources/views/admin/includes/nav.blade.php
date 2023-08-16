b71cadceac5b5a385c244bfbc80d40c68a01ba53
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-08-16 11:36:59 +0200

    add nav bar

--- resources/views/admin/includes/nav.blade.php
+++ resources/views/admin/includes/nav.blade.php
@@ -39,7 +39,7 @@
             </ul>
           </li>
 
-          {{-- <li class="nav-item dropdown">
+          <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               Technologies
             </a>
@@ -47,7 +47,7 @@
               <li><a class="dropdown-item" href="{{route('admin.technologies.index')}}">Index</a></li>
               <li><a class="dropdown-item" href="{{route('admin.technologies.create')}}">Add New Technology</a></li>
             </ul>
-          </li> --}}
+          </li>
         </ul>
 
         <ul class="navbar-nav mb-2 mb-lg-0">
