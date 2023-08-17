e8439bd112c4c7fa58609d85e995c5dc36fe35dc
alessandropecchini99 <pecchini99@gmail.com>
2023-08-17 12:05:55 +0200

    add index to api controller

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -2,10 +2,19 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+use App\Models\Post;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class PostController extends Controller
 {
-    //
+    public function index()
+    {
+        $post = Post::all();
+
+        return response()->json([
+            'success' => true,
+            'results' => $post,
+        ]);
+    }
 }
